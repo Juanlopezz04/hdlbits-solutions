5829f119ae97718dcc77a40d9b80245d26636d8b
Juanlopezz04 <jlopeznarbona@gmail.com>
2026-03-24 20:35:26 +0300

    procedures

--- 02_Verilog_Language/Procedures/Always_if.v
+++ 02_Verilog_Language/Procedures/Always_if.v
@@ -2,7 +2,7 @@
  * Build a 2-to-1 mux that chooses between a and b. Choose b if both 
  * sel_b1 and sel_b2 are true. Otherwise, choose a. Do the same twice, once 
  * using assign statements and once using a procedural if statement.
- * Web: HDLBits
+
  */
 
 module top_module(
--- 02_Verilog_Language/Procedures/Always_if2.v
+++ 02_Verilog_Language/Procedures/Always_if2.v
@@ -2,7 +2,7 @@
 * The following code contains incorrect behaviour that creates a latch. 
  * Fix the bugs so that you will shut off the computer only if it's really overheated, 
  * and stop driving if you've arrived at your destination or you need to refuel.
- * Web: HDLBits
+ 
  */
 
 module top_module (
