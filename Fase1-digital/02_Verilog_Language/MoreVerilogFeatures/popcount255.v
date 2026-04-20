7318a55d9f1cf02cb8b5ddb1517bf04abe48c64e
Juanlopezz04 <jlopeznarbona@gmail.com>
2026-04-20 12:21:31 +0300

    Solve popcount255.v - count ones in 255-bit vector using for loop

---
+++ Fase1-digital/02_Verilog_Language/MoreVerilogFeatures/popcount255.v
@@ -0,0 +1,14 @@
+// Count the number of '1' bits in a 255-bit input vector.
+// Output is an 8-bit value representing the total count (0 to 255).
+
+module top_module(
+    input [254:0] in,
+    output reg [7:0] out
+);
+    integer i;
+    always @(*) begin
+        out = 8'd0;
+        for (i = 0; i < 255; i = i + 1)
+            out = out + in[i];
+    end
+endmodule
