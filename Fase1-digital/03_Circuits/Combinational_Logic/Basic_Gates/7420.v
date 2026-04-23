09bae6023ec77c0e4ac32888e56ac78bcc52d3f1
Juanlopezz04 <jlopeznarbona@gmail.com>
2026-04-23 13:36:15 +0300

    Solve 7420.v - dual 4-input NAND gate replicating 7420 chip

---
+++ Fase1-digital/03_Circuits/Combinational_Logic/Basic_Gates/7420.v
@@ -0,0 +1,12 @@
+// Replicate the 7420 chip: two independent 4-input NAND gates.
+
+module top_module (
+    input  p1a, p1b, p1c, p1d,
+    output p1y,
+    input  p2a, p2b, p2c, p2d,
+    output p2y
+);
+    assign p1y = ~(p1a & p1b & p1c & p1d);
+    assign p2y = ~(p2a & p2b & p2c & p2d);
+
+endmodule
