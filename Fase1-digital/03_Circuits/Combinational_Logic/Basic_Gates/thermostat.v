597506f3bee9e08b1a1dfc1fa1b3a9d6d01549e2
Juanlopezz04 <jlopeznarbona@gmail.com>
2026-04-23 15:09:23 +0300

    Solve thermostat.v - heater/aircon/fan control with assign statements

---
+++ Fase1-digital/03_Circuits/Combinational_Logic/Basic_Gates/thermostat.v
@@ -0,0 +1,16 @@
+// Thermostat: heater, aircon, and fan control logic
+
+module top_module (
+    input  too_cold,
+    input  too_hot,
+    input  mode,
+    input  fan_on,
+    output heater,
+    output aircon,
+    output fan
+);
+    assign heater = mode & too_cold;
+    assign aircon = ~mode & too_hot;
+    assign fan    = heater | aircon | fan_on;
+
+endmodule
